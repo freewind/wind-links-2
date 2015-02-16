b118bae6bb81a22879e12a01ad7d8d37e4aea315
Peng.Li <peng.li@rea-group.com>
2015-02-16 20:56:02 +0800

    A react hello world

--- src/main/scala/in/freewind/windlinks/App.scala
+++ src/main/scala/in/freewind/windlinks/App.scala
@@ -1,11 +1,18 @@
 package in.freewind.windlinks
 
+import japgolly.scalajs.react.vdom.all._
+import japgolly.scalajs.react.{ReactComponentB, _}
+import org.scalajs.dom
+
 import scala.scalajs.js.JSApp
 
 object App extends JSApp {
 
+
+  val Hello = ReactComponentB[String]("Hello").render(i => h1(i)).build
+
   override def main(): Unit = {
-    println("Hello, reactjs-scala!")
+    Hello("scalajs-react") render dom.document.body
   }
 
 }
